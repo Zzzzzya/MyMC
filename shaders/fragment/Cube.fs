4f9e085f20f7a30602471000e90045c8bf952b39
zya <1733179626@qq.com>
2024-07-10 20:48:12 +0800

    update(cube Shader):update cube shader, for cube with 6 2D textures

--- shaders/fragment/Cube.fs
+++ shaders/fragment/Cube.fs
@@ -1,11 +1,17 @@
 #version 330 core
 
 in vec2 TexCoords;
+flat in int index;
 
 out vec4 FragColor;
 
-uniform sampler2D tex;
+uniform sampler2D tex[30];
+
+const int CubeCount = 6;
+vec3 colors[CubeCount] = vec3[](vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0), vec3(1.0, 1.0, 0.0),
+                                vec3(1.0, 0.0, 1.0), vec3(0.0, 1.0, 1.0));
 
 void main() {
-    FragColor = texture(tex, TexCoords);
+
+    FragColor = texture(tex[index], TexCoords);
 }
