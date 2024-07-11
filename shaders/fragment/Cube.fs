541947700e5d64e78470a08d7fbdc17d2f2e2541
zya <1733179626@qq.com>
2024-07-11 11:15:22 +0800

    update(Cube.fs): use 2D textures

--- shaders/fragment/Cube.fs
+++ shaders/fragment/Cube.fs
@@ -8,12 +8,6 @@ out vec4 FragColor;
 #define MAX_TEXTURES 30
 uniform sampler2D tex[MAX_TEXTURES];
 
-const int CubeCount = 6;
-vec3 colors[CubeCount] = vec3[](vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0), vec3(1.0, 1.0, 0.0),
-                                vec3(1.0, 0.0, 1.0), vec3(0.0, 1.0, 1.0));
-
 void main() {
-
-    // FragColor = texture(tex[index], TexCoords);
-    FragColor = vec4(colors[index], 1.0);
+    FragColor = texture(tex[index], TexCoords);
 }
