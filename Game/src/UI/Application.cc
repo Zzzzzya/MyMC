a294f7c18c59d21edb458c4712d930dd19b22132
hins <3406199643@qq.com>
2024-09-21 18:29:37 +0800

    change style of window and button

--- Game/src/UI/Application.cc
+++ Game/src/UI/Application.cc
@@ -1,17 +1,20 @@
 #include "Application.hpp"
 #include "Scene.hpp"
 
-App::App(Scene *scene) : scene(scene) {
+App::App(Scene *scene) : scene(scene)
+{
 }
 
-void App::PrepareRender() {
+void App::PrepareRender()
+{
 
     // 开始新的ImGui帧
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    switch (state) {
+    switch (state)
+    {
     case State::INIT:
         Init();
         break;
@@ -40,17 +43,16 @@ void App::PrepareRender() {
     glfwGetFramebufferSize(scene->window, &scene->display_w, &scene->display_h);
 }
 
-void App::ImGuiRender() {
+void App::ImGuiRender()
+{
     /* ImGui Render */
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void App::Init() {
-    // 创建一个窗口
-    ImGui::Begin("Example Window");
-    ImGui::Text("FPS: %.1f", scene->fps);
-    // 创建一个无边框按钮
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+void App::Init()
+{
+    // 创建一个window并设置window和buttons的样式
+    ImVec2 BtnSize = BeginWindow("Example Window", 3);
 
     /**
      * @brief 主界面渲染逻辑
@@ -60,46 +62,53 @@ void App::Init() {
      * @button Setting 设置
      *
      */
-    if (ImGui::Button("New Game!")) {
+    if (ImGui::Button("New Game!", BtnSize))
+    {
         state = State::CREATING_NEW_GAME;
         glfwSetInputMode(scene->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         scene->cursorInWindow = true;
     }
-    if (ImGui::Button("Continue Game!")) {
+    
+    ImGui::Spacing();
+    // ImGui::SetCursorPosX(startX);
+    if (ImGui::Button("Continue Game!", BtnSize))
+    {
         // TODO：继续上一次游戏
         /**
          * @brief Step 1 ： Check if there is a saved game
          * @brief Step 2 ： Load the saved game
          */
     }
-    if (ImGui::Button("Load Game!")) {
+
+    ImGui::Spacing();
+    // ImGui::SetCursorPosX(startX);
+    if (ImGui::Button("Load Game!", BtnSize))
+    {
         // TODO：加载某个游戏存档
         /**
          * @brief
          */
     }
-    ImGui::PopStyleVar();
-    ImGui::End();
+    // 关闭窗口
+    EndWindow();
 }
 
-void App::Setting() {
+void App::Setting()
+{
     // 创建一个窗口
-    ImGui::Begin("Setting Window");
-    ImGui::Text("FPS: %.1f", scene->fps);
-    // 创建一个无边框按钮
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+    ImVec2 BtnSize = BeginWindow("Setting Window", 2, true);
 
     /**
      * @brief 主界面渲染逻辑
      * @button Continue Game 继续游戏
      *
      */
-    if (ImGui::Button("Continue Game!")) {
+    if (ImGui::Button("Continue Game!", BtnSize))
+    {
         state = State::RUN;
         glfwSetInputMode(scene->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         scene->cursorInWindow = true;
     }
-    ImGui::PopStyleVar();
 
     /**
      * @brief 调节Scene中的参数
@@ -113,7 +122,6 @@ void App::Setting() {
 
     // 2. 雾效设置
     ImGui::SliderFloat("fogDensity", &scene->fogDensity, 0.0f, 0.1f);
-
     ImGui::SliderFloat("Sun.x", &scene->SunLightVec.x, -1.0f, 1.0f);
     ImGui::SliderFloat("Sun.y", &scene->SunLightVec.y, -1.0f, 1.0f);
     ImGui::SliderFloat("Sun.z", &scene->SunLightVec.z, -1.0f, 1.0f);
@@ -122,14 +130,18 @@ void App::Setting() {
     ImGui::SliderFloat("Sun.front.z", &scene->SunPosition.z, -100.0f, 10.0f);
     ImGui::SliderFloat("shadowbias", &scene->shadowBias, .0f, 2.f);
 
-    if (ImGui::Button("Back")) {
+    if (ImGui::Button("Back", BtnSize))
+    {
         state = State::WAITING;
     }
-    ImGui::End();
+
+    EndWindow();
 }
 
-void App::Run() {
-    if (scene->showFPS) {
+void App::Run()
+{
+    if (scene->showFPS)
+    {
         ImGui::Begin("FPS");
         ImGui::Text("FPS: %.1f", scene->fps);
         ImGui::Text("Selected Block: %d", scene->player->CurBlockID);
@@ -137,46 +149,53 @@ void App::Run() {
     }
 }
 
-void App::Waiting() {
+void App::Waiting()
+{
     // 创建一个窗口
-    ImGui::Begin("Example Window");
-    ImGui::Text("FPS: %.1f", scene->fps);
-    // 创建一个无边框按钮
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+    ImVec2 BtnSize = BeginWindow("PAUSE", 3);
 
     /**
      * @brief 主界面渲染逻辑
      * @button Continue Game 继续游戏
      *
      */
-    if (ImGui::Button("Continue Game!")) {
+    if (ImGui::Button("Continue Game!", BtnSize))
+    {
         state = State::RUN;
         glfwSetInputMode(scene->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         scene->cursorInWindow = true;
     }
-    ImGui::PopStyleVar();
 
-    if (ImGui::Button("Settings")) {
+    ImGui::Spacing();
+    if (ImGui::Button("Settings", BtnSize))
+    {
         state = State::SETTING;
     }
 
-    if (ImGui::Button("Save Game")) {
+    ImGui::Spacing();
+    if (ImGui::Button("Save Game", BtnSize))
+    {
         // TODO：保存游戏
         /**
          * @brief
          */
 
         std::ofstream outFile("savegame.txt");
-        if (!outFile.is_open()) {
+        if (!outFile.is_open())
+        {
             std::cerr << "无法打开文件进行写操作" << std::endl;
             return;
         }
 
         auto map = *scene->map->_map;
-        for (const auto &layer : map) {
-            for (const auto &row : layer) {
-                for (const auto &cube : row) {
-                    if (cube != nullptr) {
+        for (const auto &layer : map)
+        {
+            for (const auto &row : layer)
+            {
+                for (const auto &cube : row)
+                {
+                    if (cube != nullptr)
+                    {
                         outFile << cube->type() << cube->ID() << std::endl;
                     }
                 }
@@ -186,16 +205,19 @@ void App::Waiting() {
         outFile.close();
         std::cout << "游戏已保存" << std::endl;
     }
-    ImGui::End();
+
+    EndWindow();
 }
 
-void App::CreatingNewGame() {
+void App::CreatingNewGame()
+{
     // 创建一个窗口
     ImGui::Begin("Example Window");
 
     ImGui::Text("CREATING NEW WORLD! %d", 1);
 
-    if (scene->bMapReady) {
+    if (scene->bMapReady)
+    {
         state = State::RUN;
     }
 
@@ -203,7 +225,78 @@ void App::CreatingNewGame() {
     ImGui::End();
 }
 
-void App::Picture() {
+void App::Picture()
+{
     ImGui::Begin("Picture Mode");
+    ImGui::End();
+}
+
+int App::GetSceneWidth()
+{
+    return scene->imageWidth;
+}
+
+int App::GetSceneHeight()
+{
+    return scene->imageHeight;
+}
+
+ImVec2 App::BeginWindow(std::string title, int BtnCount, bool setting)
+{
+    const char *cstr = title.c_str();
+    // 创建一个窗口, 大小和windows窗口一致
+    ImGui::SetNextWindowSize(ImVec2(GetSceneWidth(), GetSceneHeight()));
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::Begin(cstr, NULL, ImGuiWindowFlags_NoResize |            
+                ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+                
+    ImGui::Text("FPS: %.1f", scene->fps);
+
+    // 设置无边框
+    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+
+    // 设置buttons样式
+    float ButtonWidth = 500.f;
+    float ButtonHeight = 50.f;
+    // 设置button之间的间距
+    float SpaceBetweenButtons = 30.f;
+    float TotalBtnHeight = (ButtonHeight + SpaceBetweenButtons) * BtnCount;
+    float startY = (GetSceneHeight() - TotalBtnHeight) / 2.f;
+    float startX = (GetSceneWidth() - ButtonWidth) * 0.5f;
+
+    // 设置样式
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ItemSpacing = ImVec2(0, SpaceBetweenButtons);
+    style.FrameRounding = 10.0f;  // buttons 圆角
+    
+   
+   
+    if(!setting)
+    {
+         // 设置字体大小
+        ImGui::SetWindowFontScale(2.f);
+
+        ImGui::SetCursorPosX(startX);
+        ImGui::SetCursorPosY(startY);
+    }
+    else
+    {
+        ImGui::SetWindowFontScale(1.5f);
+
+        ImGui::SetCursorPosX(180.f);
+        ImGui::SetCursorPosY(100.f);
+    }
+    
+
+    ImGui::BeginGroup();
+
+    return ImVec2(ButtonWidth, ButtonHeight);
+}
+
+void App::EndWindow()
+{
+    ImGui::EndGroup();
+    ImGui::PopStyleVar();
+    ImGui::SetWindowFontScale(1.f);
     ImGui::End();
 }
