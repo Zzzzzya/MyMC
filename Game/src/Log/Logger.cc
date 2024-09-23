1a86eb0b63fa042625bc647dabb3c73b5642e409
Gresham <greshame@hust.edu.cn>
2024-09-24 01:31:31 +0800

    fix(log):finish log class's constuctor and destructor

--- Game/src/Log/Logger.cc
+++ Game/src/Log/Logger.cc
@@ -2,6 +2,10 @@
 
 spdlog::level::level_enum Loggers::global_level = spdlog::level::info;
 
+Loggers::Loggers() {}
+
+Loggers::~Loggers() {}
+
 spdlog::level::level_enum Loggers::getGlobalLevel()
 {
     return global_level;
